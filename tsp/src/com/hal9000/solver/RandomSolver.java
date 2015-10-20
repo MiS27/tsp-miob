f0baebe4e78d0e46af38ec099a6ef17852e70ed9
MiS27 <kontakt@michalslusarczyk.pl>
2015-10-21 00:01:30 +0200

    RandomSolver

---
+++ tsp/src/com/hal9000/solver/RandomSolver.java
@@ -0,0 +1,28 @@
+package com.hal9000.solver;
+
+import com.hal9000.data.TSPInstance;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * Created by rt on 19.10.15.
+ */
+public class RandomSolver implements Solver {
+
+    private Solution solution;
+
+    public RandomSolver(TSPInstance problem) {
+        ArrayList<Integer> sequence = new ArrayList<>(problem.getDim());
+        for (int i = 0; i < problem.getDim(); i++) {
+            sequence.add(i);
+        }
+        Collections.shuffle(sequence);
+        solution = new Solution(sequence, problem);
+    }
+
+    public Solution solve() {
+        return solution;
+    }
+
+}
--- tsp/src/com/hal9000/solver/Solver.java
+++ tsp/src/com/hal9000/solver/Solver.java
@@ -1,10 +1,9 @@
 package com.hal9000.solver;
 
-import com.hal9000.data.TSPInstance;
 
 /**
  * Created by rt on 19.10.15.
  */
 public interface Solver {
-    void perform(TSPInstance instance);
+    Solution solve();
 }
