9a721d3a072c7c0b3e86548f8b69496d2e3b51ba
MiS27 <kontakt@michalslusarczyk.pl>
2015-10-21 01:22:52 +0200

    Heuristic fix

--- tsp/src/com/hal9000/solver/HeuristicSolver.java
+++ tsp/src/com/hal9000/solver/HeuristicSolver.java
@@ -24,24 +24,25 @@ public class HeuristicSolver implements Solver {
 
         int last = 0;
         for (int i = 1; i < problem.getDim(); i++) {
-            solution.add(getClosest(last, sequence));
+            last = getClosest(last, sequence);
+            solution.add(last);
         }
         this.solution = new Solution(solution, problem);
     }
 
     private int getClosest(int last, List<Integer> sequence) {
         double distance;
-        int best = 0;
+        int bestIdx = 0;
         double bestDistance = problem.getDistance(last, sequence.get(0));
         for (int i = 1; i < sequence.size(); i++) {
             distance = problem.getDistance(last, sequence.get(i));
             if (distance < bestDistance) {
                 bestDistance = distance;
-                best = i;
+                bestIdx = i;
             }
         }
-        sequence.remove(best);
-        return best;
+        sequence.remove(bestIdx);
+        return bestIdx;
     }
 
     public Solution solve() {
