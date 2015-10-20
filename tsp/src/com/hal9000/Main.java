9bde28be2502c608cc2eb06a29624fe4d3253f23
m87 <m87blackhole@gmail.com>
2015-10-20 20:29:03 +0200

    simple random

--- tsp/src/com/hal9000/Main.java
+++ tsp/src/com/hal9000/Main.java
@@ -4,9 +4,9 @@ import com.hal9000.parsers.Parser;
 import com.hal9000.parsers.SimpleParser;
 import com.hal9000.data.TSPInstance;
 import com.hal9000.random.SimpleRandom;
+import com.hal9000.thirdParty.RandomHotBits;
 import com.hal9000.time.SimpleTimer;
 import com.hal9000.time.Timer;
-import com.hal9000.random.*;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -30,7 +30,7 @@ public class Main {
         timer.start();
 
         SimpleRandom rand = new SimpleRandom(1);
-        randomHotBits rr = new randomHotBits();
+        RandomHotBits rr = new RandomHotBits();
         for(int i=0; i < 1000; i++) {
             System.out.print(Math.abs(rr.nextInt()%100) + "|");
         }
