42822cec394c8b2a7f3a707214807b65d9261854
m87 <m87blackhole@gmail.com>
2015-11-01 11:24:36 +0100

    report

--- tsp/src/com/hal9000/env/Report.java
+++ tsp/src/com/hal9000/env/Report.java
@@ -3,17 +3,25 @@ package com.hal9000.env;
 import com.hal9000.data.TSPInstance;
 import com.hal9000.solver.Solution;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class Report{
-    public Map<String, Map<Integer, Map.Entry<TSPInstance, Solution>>> solutions;
+    public Map<String, Map<TSPInstance, List<Solution>>> solutions;
 
     public Report(){
         solutions = new TreeMap<>();
     }
 
+    public void addToReport(String key, TSPInstance instance, Solution solution){
+        if(!solutions.containsKey(key)) solutions.put(key, new TreeMap<TSPInstance, List<Solution>>());
+        if(!solutions.get(key).containsKey(instance))  solutions.get(key).put(instance,new ArrayList<Solution>());
 
+        solutions.get(key).get(instance).add(solution);
+
+    }
 
     public void dump(OutputFormatter outputFormatter){
     }
