efb2cde628e28056206b83783845d9dd62d0ae21
m87 <m87blackhole@gmail.com>
2015-11-08 10:56:39 +0100

    report

--- tsp/src/com/hal9000/env/SimpleReport.java
+++
@@ -1,8 +0,0 @@
-package com.hal9000.env;
-
-public class SimpleReport extends Report{
-    @Override
-    public void dump(String file, String sep) {
-        //TODO average time, value ....
-    }
-}
