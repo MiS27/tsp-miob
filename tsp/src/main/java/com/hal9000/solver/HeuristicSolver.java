3410be97c028213359828778e752fd55c23407c7
MiS27 <kontakt@michalslusarczyk.pl>
2015-11-17 21:59:11 +0100

    better heura

--- tsp/src/main/java/com/hal9000/solver/HeuristicSolver.java
+++ tsp/src/main/java/com/hal9000/solver/HeuristicSolver.java
@@ -10,40 +10,54 @@ public class HeuristicSolver implements Solver {
 
     private final TSPInstance problem;
     private final Solution solution;
+    private ArrayList<Integer> solutionSequence;
+    private double best;
 
     public HeuristicSolver(TSPInstance problem) {
         this.problem = problem;
-        ArrayList<Integer> solution = new ArrayList<>(problem.getDim());
+
+        List<Integer> fullSequence = new LinkedList<>();
+        for (int i = 0; i < problem.getDim(); i++) {
+            fullSequence.add(i);
+        }
 
         List<Integer> sequence = new LinkedList<>();
-        for (int i = 1; i < problem.getDim(); i++) {
-            sequence.add(i);
+        for (int i = 0; i < problem.getDim(); i++) {
+            sequence.clear();
+            sequence.addAll(fullSequence);
+            generateSolution(sequence, i);
         }
-        solution.add(0);
 
-        int last = 0;
-        for (int i = 1; i < problem.getDim(); i++) {
-            last = getClosest(last, sequence);
-            solution.add(last);
-        }
-        this.solution = new Solution(solution, problem);
+        this.solution = new Solution(solutionSequence, problem);
     }
 
-    private int getClosest(int last, List<Integer> sequence) {
-        double distance;
-        int bestIdx = 0;
-        int result ;
-        double bestDistance = problem.getDistance(last, sequence.get(0));
-        for (int i = 1; i < sequence.size(); i++) {
-            distance = problem.getDistance(last, sequence.get(i));
-            if (distance < bestDistance) {
-                bestDistance = distance;
-                bestIdx = i;
+    private void generateSolution(List<Integer> sequence, int start) {
+        ArrayList<Integer> tmpSolution = new ArrayList<>(problem.getDim());
+        double score = 0.0;
+        tmpSolution.add(start);
+        int last = start;
+        for (int i = 1; i < problem.getDim(); i++) {
+            double distance;
+            int bestIdx = 0;
+            double bestDistance = problem.getDistance(last, sequence.get(0));
+            for (int j = 1; j < sequence.size(); j++) {
+                distance = problem.getDistance(last, sequence.get(j));
+                if (distance < bestDistance) {
+                    bestDistance = distance;
+                    bestIdx = j;
+                }
             }
+            score += bestDistance;
+            last = sequence.get(bestIdx);
+            tmpSolution.add(last);
+            sequence.remove(last);
         }
-        result = sequence.get(bestIdx);
-        sequence.remove(sequence.get(bestIdx));
-        return result;
+        score += problem.getDistance(last, start);
+
+        if (solution == null || score < best) {
+            solutionSequence = tmpSolution;
+            best = score;
+        }
     }
 
     public Solution solve(Arg argument) {
