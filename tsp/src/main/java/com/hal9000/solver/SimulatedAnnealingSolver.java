502e74a1a26b7e1d7623a996314fa9218eab472e
MiS27 <kontakt@michalslusarczyk.pl>
2015-12-17 01:46:42 +0100

    Annealing memorize best solution

--- tsp/src/main/java/com/hal9000/solver/SimulatedAnnealingSolver.java
+++ tsp/src/main/java/com/hal9000/solver/SimulatedAnnealingSolver.java
@@ -25,6 +25,12 @@ public class SimulatedAnnealingSolver extends RandomSolver {
 
     private int stop;
 
+    private ArrayList<Integer> bestSequence;
+
+    private double bestCost;
+
+    private double currentCost;
+
     public SimulatedAnnealingSolver(TSPInstance problem, int lk, double alpha, int stop) {
         super(problem);
         this.problem = problem;
@@ -32,6 +38,68 @@ public class SimulatedAnnealingSolver extends RandomSolver {
         this.alpha = alpha;
         this.stop = stop;
         random = new Random();
+    }
+
+    public Solution solve(Arg argument) {
+        int counter = 0;
+        int steps = 0;
+        c = generateTemperature((Opt) argument);
+        while (counter < stop) {
+            if (!step(argument)) {
+                counter++;
+            } else {
+                counter = 0;
+            }
+            steps++;
+            c = (int)(alpha * c);
+        }
+
+        solution.setSteps(steps);
+        solution.setSolution(bestSequence);
+        return solution;
+    }
+
+    private boolean step(Arg argument) {
+        boolean improved = false;
+        for (int l = 0; l < lk; l++) {
+            if (tryMove((Opt) argument)) {
+                improved = true;
+            }
+            solution.setChecked(solution.getChecked()+1);
+        }
+
+        return improved;
+    }
+
+    private boolean tryMove(Opt argument) {
+        boolean improved = false;
+        int i = random.nextInt(problem.getDim());
+        // -1 because i != j
+        int j = random.nextInt(problem.getDim() - 1);
+
+        if (j < i) {
+            int tmp = j;
+            j = i;
+            i = tmp;
+        } else {
+            // +1 because i != j
+            j++;
+        }
+
+        double delta = argument.getMoveDelta(i, j, solution);
+
+        if (delta  < -Environment.eps) {
+            argument.move(i, j, solution);
+            currentCost += delta;
+            if (currentCost < bestCost) {
+                bestCost = currentCost;
+                bestSequence = new ArrayList<>(solution.getSolution());
+            }
+            improved = true;
+        } else if (Math.exp(-delta/c) > Math.random()) {
+            argument.move(i, j, solution);
+        }
+        return improved;
     }
 
     private int generateTemperature(Opt argument) {
@@ -69,62 +137,6 @@ public class SimulatedAnnealingSolver extends RandomSolver {
             result = (int) Math.ceil(-mean/Math.log(1 - 0.1/(1-deltaZero/(solutions*moves))));
         }
         return result;
-    }
-
-    public Solution solve(Arg argument) {
-        int counter = 0;
-        int steps = 0;
-        c = generateTemperature((Opt) argument);
-        while (counter < stop) {
-            if (!step(argument)) {
-                counter++;
-            } else {
-                counter = 0;
-            }
-            steps++;
-            c = (int)(alpha * c);
-        }
-
-        solution.setSteps(steps);
-        return solution;
-    }
-
-    private boolean step(Arg argument) {
-        boolean improved = false;
-        for (int l = 0; l < lk; l++) {
-            if (tryMove((Opt) argument)) {
-                improved = true;
-            }
-            solution.setChecked(solution.getChecked()+1);
-        }
-
-        return improved;
-    }
-
-    private boolean tryMove(Opt argument) {
-        boolean improved = false;
-        int i = random.nextInt(problem.getDim());
-        // -1 because i != j
-        int j = random.nextInt(problem.getDim() - 1);
-
-        if (j < i) {
-            int tmp = j;
-            j = i;
-            i = tmp;
-        } else {
-            // +1 because i != j
-            j++;
-        }
-
-        double delta = argument.getMoveDelta(i, j, solution);
-
-        if (delta  < -Environment.eps) {
-            argument.move(i, j, solution);
-            improved = true;
-        } else if (Math.exp(-delta/c) > Math.random()) {
-            argument.move(i, j, solution);
-        }
-        return improved;
     }
 
 }
